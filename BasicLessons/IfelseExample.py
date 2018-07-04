4bf7362424bb6b471459360a933d2a56bfd21d2a
Thirumurthi <thirumurthi.s@gmail.com>
2018-07-03 23:39:25 -0700

    commit issued with changes uing if else loop in different variation for input entered by user.

---
+++ BasicLessons/IfelseExample.py
@@ -0,0 +1,21 @@
+
+x=int(input("Enter a number"))
+
+if x>5:
+    print ("The input is greater than 5")
+else:
+    pring ("input less")
+
+if x <10:
+    print ("less than 10")
+else:
+        if x == 10:
+            print ("equal to 10")
+
+if x <10:
+    print("less than 10")
+elif x > 10:
+        print ("greater than 10")
+elif x == 10:
+    print ("equal to 10")
+        
