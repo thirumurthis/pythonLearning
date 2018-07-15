11f12bd54e843978572046c8a4d6e8ff2fa63def
Thirumurthi <thirumurthi.s@gmail.com>
2018-07-15 08:22:40 -0700

    commit with updated readme

---
+++ BasicLessons/Oops_concept.py
@@ -0,0 +1,4 @@
+################## IMPORTATN : OBJECT ORIENTED PROGRAMMING CONCEPT #######
+
+# imperative programming (using statements, loops, and functions as subroutines),
+# functional programming (using pure functions, higher-order functions, and recursion).
---
+++ BasicLessons/README.md
@@ -0,0 +1,6 @@
+In order to execute the files,
+install the python version
+
+for exection of each py file
+use > python <filename>.py
+
